baf067337569e088125bcaaf9778bd8dbe01ed67
Kornil17 <dimitra4@mail.ru>
2024-10-23 00:53:12 +0300

    fixed structure 3

--- Timer/setup.py
+++ Timer/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='TimerLib',
+    name='Timer',
     version='1.1.0',
     packages=find_packages(),
     description='Timer library for synchronous and asynchronous functions',
--- build/lib/Timer/__init__.py
+++
@@ -1,5 +0,0 @@
-from logging import basicConfig, DEBUG, getLogger
-
-basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = getLogger(__name__)
-logger.setLevel(DEBUG)  # Установка уровня логирования
