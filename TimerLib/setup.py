0e0e7ba03aa9e6b2c0402cdb79a633c7b42b31f1
Kornil17 <dimitra4@mail.ru>
2024-10-23 00:55:38 +0300

    fixed structure 4

--- TimerLib/setup.py
+++ TimerLib/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Timer',
+    name='TimerLib',
     version='1.1.0',
     packages=find_packages(),
-    description='Timer library for synchronous and asynchronous functions',
+    description='TimerLib library for synchronous and asynchronous functions',
     author='Kornil17',
     author_email='Dimitra4@mail.ru',
     url='https://github.com/Kornil17/TimerLib.git',  # Укажите URL вашего репозитория
